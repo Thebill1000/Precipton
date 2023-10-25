d1255d2ef5d9d55dd2c3cf7a919dc46d10f72723
Thebill1000 <williamsbilljnr100@gmail.com>
2023-10-25 10:47:06 -0700

    flask app edited to include new sms features

--- App/app.py
+++ App/app.py
@@ -1,39 +1,50 @@
-#!/usr/bin/python3
-
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 from twilio.rest import Client
 import mandrill
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patients.db'
-db = SQLAlchemy(app)
-
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    medication = db.Column(db.String(120), nullable=False)
-    phone_number = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-
-db.create_all()
-
-# Twilio credentials
+# Twilio and Mandrill configurations
 TWILIO_ACCOUNT_SID = 'ACb78b0fd70d33ac6cabf5308a70f73c2f'
-TWILIO_AUTH_TOKEN = '5a121b6623138bf8b9da439ad61e261d'
-TWILIO_PHONE_NUMBER = '+2347063808082'
-
-# Mandrill API key
+TWILIO_AUTH_TOKEN = 'e55bcd7ead88c6e8e756692f4a4389b7'
+TWILIO_PHONE_NUMBER = '+15173250595'
 MANDRILL_API_KEY = 'md-ae8bYGk7lI679BT04SfNJQ'
 
+twilio_client = Client(ACb78b0fd70d33ac6cabf5308a70f73c2f, e55bcd7ead88c6e8e756692f4a4389b7)
+mandrill_client = mandrill.Mandrill(md-ae8bYGk7lI679BT04SfNJQ)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    phone_or_email = request.form['phoneOrEmail']
+    medication = request.form['medication']
+    pills_count = request.form['pillsCount']
+    medication_duration = request.form['medicationDuration']
+    first_dose_time = request.form['firstDoseTime']
+    reminder_interval = request.form['reminderInterval']
+
+    # Save patient data to the database (you might want to add this part)
+
+    # Send SMS notification
+    sms_message = f"Hello {name}, it's time to take your medication: {medication}. Please take {pills_count} pills."
+    send_sms(phone_or_email, sms_message)
+
+    # Send email notification (if email is provided)
+    if '@' in phone_or_email:
+        email_subject = "Medication Reminder"
+        email_message = f"Hello {name},\n\nIt's time to take your medication: {medication}.\n\nPlease take {pills_count} pills.\n\nBest regards,\nYour Healthcare Team"
+        send_email(phone_or_email, email_subject, email_message)
+
+    return "Notifications sent successfully!"
+
 def send_sms(phone_number, message):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    client.messages.create(to=phone_number, from_=TWILIO_PHONE_NUMBER, body=message)
+    twilio_client.messages.create(to=phone_number, from_=TWILIO_PHONE_NUMBER, body=message)
 
 def send_email(recipient_email, subject, message):
-    mandrill_client = mandrill.Mandrill(MANDRILL_API_KEY)
     message_data = {
         'from_email': 'your_email@example.com',
         'to': [{'email': recipient_email}],
@@ -41,45 +52,6 @@ def send_email(recipient_email, subject, message):
         'text': message,
     }
     mandrill_client.messages.send(message=message_data, async=False)
-
-# Routes
-
-@app.route('/add_patient', methods=['POST'])
-def add_patient():
-    data = request.get_json()
-    name = data['name']
-    medication = data['medication']
-    phone_number = data['phone_number']
-    email = data['email']
-
-    new_patient = Patient(name=name, medication=medication, phone_number=phone_number, email=email)
-    db.session.add(new_patient)
-    db.session.commit()
-
-    return jsonify({'message': f'Patient {name} added successfully!'}), 201
-
-@app.route('/schedule_reminder', methods=['POST'])
-def schedule_reminder():
-    data = request.get_json()
-    name = data['name']
-    reminder_time = data['reminder_time']  # Implement your scheduling logic here
-    patient = Patient.query.filter_by(name=name).first()
-
-    if patient:
-        medication = patient.medication
-        phone_number = patient.phone_number
-        email = patient.email
-        message = f"Reminder: It's time to take your {medication}."
-
-        # Schedule SMS and email reminders here
-        send_sms(phone_number, message)
-        send_email(email, 'Medication Reminder', message)
-
-        return jsonify({'message': f'Reminder scheduled for {name}!'}), 200
-    else:
-        return jsonify({'error': f'Patient {name} not found!'}), 404
-
-# ... (other routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
